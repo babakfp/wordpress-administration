e6bb991730df9d8b23dd717778c75d890655d262
babakfp <44144724+babakfp@users.noreply.github.com>
2022-08-08 11:47:51 -0700

    stuff

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,9 +11,3 @@
 		<slot />
 	</main>
 </div>
-
-<!-- TODO: This is for PostType layout -->
-<!-- <div class="container [ flex items-center justify-between py-6 ] [ border-b border-white border-opacity-5 ]">
-	<h1 class="text-xl font-medium">Templates</h1>
-	<a class="btn btn--sm btn--brand" href="/">Add New</a>
-</div> -->
